cba8caeed1cb7a2c10b0e900c818772f8815d738
HJ Sievert <hj.sievert@gmail.com>
2020-02-13 16:52:10 -0800

    Implemented drug stack

--- Screens/Plans/pDrugs.js
+++ Screens/Plans/pDrugs.js
@@ -6,13 +6,16 @@ import {
   TouchableHighlight
 } from 'react-native';
 import { Icon } from 'react-native-elements';
+import pSearch from '../Plans/pSearch';
+import pSelect from '../Plans/pSelect';
+import pPick from '../Plans/pPick';
 import { createStackNavigator } from '@react-navigation/stack';
 
 function DrugScreen() {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Drugs</Text>
+      <Text>Drugs Main</Text>
     </View>)
 }
 
@@ -33,7 +36,142 @@ function Drugs() {
           headerRight: () => (
             <View style={styles.innerContainer}>
               <TouchableHighlight
+                onPress={() => navigation.navigate('pSearch')}
+              >
+                <View style={[styles.touch, { justifyContent: 'right' }]}>
+                  <Icon
+                    name={'ios-arrow-forward'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+          headerLeft: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
                 onPress={() => navigation.navigate('HomeScreen')}
+              >
+                <View style={[styles.touch, { justifyContent: 'left' }]}>
+                  <Icon
+                    name={'ios-arrow-back'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+        })} />
+
+      <Stack.Screen
+        name="pSearch"
+        component={pSearch}
+        options={({ navigation, route }) => ({
+          title: 'Search Drugs',
+          headerTitle: "Drug Search",
+          headerStyle: { backgroundColor: '#405ce8' },
+          headerTitleStyle: { fontWeight: 'normal' },
+          headerTintColor: 'white',
+          headerRight: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
+                onPress={() => navigation.navigate('pPick')}
+              >
+                <View style={[styles.touch, { justifyContent: 'right' }]}>
+                  <Icon
+                    name={'ios-arrow-forward'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+          headerLeft: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
+                onPress={() => navigation.navigate('DrugScreen')}
+              >
+                <View style={[styles.touch, { justifyContent: 'left' }]}>
+                  <Icon
+                    name={'ios-arrow-back'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+        })} />
+
+      <Stack.Screen
+        name="pSelect"
+        component={pSelect}
+        options={({ navigation, route }) => ({
+          title: 'Select Drugs',
+          headerTitle: "Drug Selection",
+          headerStyle: { backgroundColor: '#405ce8' },
+          headerTitleStyle: { fontWeight: 'normal' },
+          headerTintColor: 'white',
+          headerLeft: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
+                onPress={() => navigation.navigate('DrugScreen')}
+              >
+                <View style={[styles.touch, { justifyContent: 'left' }]}>
+                  <Icon
+                    name={'ios-arrow-back'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+        })} />
+
+      <Stack.Screen
+        name="pPick"
+        component={pPick}
+        options={({ navigation, route }) => ({
+          title: 'Pick Base Drugs',
+          headerTitle: "Drug Picker",
+          headerStyle: { backgroundColor: '#405ce8' },
+          headerTitleStyle: { fontWeight: 'normal' },
+          headerTintColor: 'white',
+          headerLeft: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
+                onPress={() => navigation.navigate('DrugScreen')}
+              >
+                <View style={[styles.touch, { justifyContent: 'left' }]}>
+                  <Icon
+                    name={'ios-arrow-back'}
+                    type={'ionicon'}
+                    color={'white'}
+                    size={24}
+                    style={{ padding: 5 }}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ),
+          headerRight: () => (
+            <View style={styles.innerContainer}>
+              <TouchableHighlight
+                onPress={() => navigation.navigate('pSelect')}
               >
                 <View style={[styles.touch, { justifyContent: 'right' }]}>
                   <Icon
@@ -48,6 +186,7 @@ function Drugs() {
             </View>
           ),
         })} />
+
     </Stack.Navigator>
   );
 }
---
+++ Screens/Plans/pSearch.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+
+function pSearch() {
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Drug Search</Text>
+    </View>)
+}
+
+export default pSearch;
