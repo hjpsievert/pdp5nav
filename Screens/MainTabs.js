22e04c3044e56235a4c56898c37f68b2e02b352d
HJ Sievert <hj.sievert@gmail.com>
2020-02-21 21:07:24 -0800

    Converted to Class for badge tracking

--- Screens/MainTabs.js
+++ Screens/MainTabs.js
@@ -1,125 +1,158 @@
 import React from 'react';
 import {
   View,
-  Text, Platform
+  Text,
+  Platform,
+  Dimensions
 } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as Dispatch from '../Redux/Dispatches';
 import { Icon } from 'react-native-elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import size from 'lodash/size';
 import pHomeStack from './Plans/pHomeStack';
 import pDrugStack from './Plans/pDrugStack';
 import pPlanStack from './Plans/pPlanStack';
 
-export default function MainTab() {
+export class MainTabs extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      flag: Dimensions.get('window').width * 1000 + Dimensions.get('window').height,
+      adjust: Dimensions.get('window').width > Dimensions.get('window').height && Platform.OS !== 'web'
+    }
+  }
 
-  let Tab = createBottomTabNavigator();
+  render() {
+    let Tab = createBottomTabNavigator();
+    const { drugCount, planCount } = this.props;
 
-  return (
-    <Tab.Navigator
-      showIcon={true}
-      // lazy={false}
-      labelStyle={{
-        fontSize: 10,
-        fontWeight: 'bold',
-        paddingTop: Platform.OS === 'ios' ? 0 : -5,
-      }}
-      tabStyle={{
-        paddingTop: Platform.OS === 'ios' ? 0 : -5,
-      }}
-      style={{
-        backgroundColor: 'white',
-        height: 45,
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={pHomeStack}
-        options={() => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return (
-              <Icon
-                name={'home'}
-                type={'material-community'}
-                size={Platform.OS === 'ios' ? size + 4 : size}
-                color={color}
-              />
-            )
-          }
-        })}
-      />
-      <Tab.Screen
-        name="Drugs"
-        component={pDrugStack}
-        options={() => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return (
-              <View style={{ width: 24, height: 24, margin: 5 }}>
+    return (
+      <Tab.Navigator
+        showIcon={true}
+        // lazy={false}
+        labelStyle={{
+          fontSize: 10,
+          fontWeight: 'bold',
+          paddingTop: Platform.OS === 'ios' ? 0 : -5,
+        }}
+        tabStyle={{
+          paddingTop: Platform.OS === 'ios' ? 0 : -5,
+        }}
+        style={{
+          backgroundColor: 'white',
+          height: 45,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={pHomeStack}
+          options={() => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
                 <Icon
-                  name={'pill'}
+                  name={'home'}
                   type={'material-community'}
                   size={Platform.OS === 'ios' ? size + 4 : size}
                   color={color}
                 />
-                {
-                  <View
-                    style={{
-                      // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
-                      position: 'absolute',
-                      right: -6,
-                      top: -3,
-                      backgroundColor: 'red',
-                      borderRadius: 6,
-                      width: 12,
-                      height: 12,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
-                      {8}
-                    </Text>
-                  </View>
-                }
-              </View>)
-          }
-        })}
-      />
-      <Tab.Screen
-        name="Plans"
-        component={pPlanStack}
-        options={() => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return (
-              <View style={{ width: 24, height: 24, margin: 5 }}>
-                <Icon
-                  name={'assessment'}
-                  type={'material'}
-                  size={Platform.OS === 'ios' ? size + 4 : size}
-                  color={color}
-                />
-                {
-                  <View
-                    style={{
-                      // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
-                      position: 'absolute',
-                      right: -6,
-                      top: -3,
-                      backgroundColor: 'red',
-                      borderRadius: 6,
-                      width: 12,
-                      height: 12,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
-                      {21}
-                    </Text>
-                  </View>
-                }
-              </View>)
-          }
-        })}
-      />
-    </Tab.Navigator >
-  ); 
+              )
+            }
+          })}
+        />
+        <Tab.Screen
+          name="Drugs"
+          component={pDrugStack}
+          options={() => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
+                <View style={{ width: 24, height: 24, margin: 5 }}>
+                  <Icon
+                    name={'pill'}
+                    type={'material-community'}
+                    size={Platform.OS === 'ios' ? size + 4 : size}
+                    color={color}
+                  />
+                  {drugCount > 0 &&
+                    <View
+                      style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: -6,
+                        top: -3,
+                        backgroundColor: 'red',
+                        borderRadius: 6,
+                        width: 12,
+                        height: 12,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                        {drugCount}
+                      </Text>
+                    </View>
+                  }
+                </View>)
+            }
+          })}
+        />
+        <Tab.Screen
+          name="Plans"
+          component={pPlanStack}
+          options={() => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
+                <View style={{ width: 24, height: 24, margin: 5 }}>
+                  <Icon
+                    name={'assessment'}
+                    type={'material'}
+                    size={Platform.OS === 'ios' ? size + 4 : size}
+                    color={color}
+                  />
+                  {planCount > 0 &&
+                    <View
+                      style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: -6,
+                        top: -3,
+                        backgroundColor: 'red',
+                        borderRadius: 6,
+                        width: 12,
+                        height: 12,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                        {planCount}
+                      </Text>
+                    </View>
+                  }
+                </View>)
+            }
+          })}
+        />
+      </Tab.Navigator >
+    );
+  }
 }
+
+MainTabs.propTypes = {
+  drugCount: PropTypes.number.isRequired,
+  planCount: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    drugCount: size(state.myDrugs) ? size(state.myDrugs) : 7,
+    planCount: size(state.myPlans) ? size(state.myPlans) : 11,
+  }
+}
+
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainTabs);
