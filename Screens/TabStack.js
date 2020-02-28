ca9a21da4e1e2ea5874cf9e0ad9c812f81990a6a
HJ Sievert <hj.sievert@gmail.com>
2020-02-27 19:47:53 -0800

    removed onPress for refresh of pDrugs

--- Screens/TabStack.js
+++ Screens/TabStack.js
@@ -67,38 +67,35 @@ export class TabStack extends React.Component {
           options={({ navigation }) => ({
             tabBarIcon: ({ focused, color, size }) => {
               return (
-                <TouchableHighlight
-                  onPress={() => navigation.navigate('Drugs', { screen: 'pDrugs', params: { refresh: true } })}
-                >
-                  <View style={{ width: 24, height: 24, margin: 5 }}>
-                    <Icon
-                      name={'pill'}
-                      type={'material-community'}
-                      size={Platform.OS === 'ios' ? size + 4 : size}
-                      color={color}
-                    />
-                    {drugCount > 0 &&
-                      <View
-                        style={{
-                          // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
-                          position: 'absolute',
-                          right: -6,
-                          top: -3,
-                          backgroundColor: 'red',
-                          borderRadius: 6,
-                          width: 12,
-                          height: 12,
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                        }}
-                      >
-                        <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
-                          {drugCount}
-                        </Text>
-                      </View>
-                    }
-                  </View>
-                </TouchableHighlight>)
+                <View style={{ width: 24, height: 24, margin: 5 }}>
+                  <Icon
+                    name={'pill'}
+                    type={'material-community'}
+                    size={Platform.OS === 'ios' ? size + 4 : size}
+                    color={color}
+                  />
+                  {drugCount > 0 &&
+                    <View
+                      style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: -6,
+                        top: -3,
+                        backgroundColor: 'red',
+                        borderRadius: 6,
+                        width: 12,
+                        height: 12,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                        {drugCount}
+                      </Text>
+                    </View>
+                  }
+                </View>
+              )
             }
           })}
         />
