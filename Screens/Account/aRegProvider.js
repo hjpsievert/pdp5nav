c0f8c5c598add9c717768edad50b5337400d05e2
HJ Sievert <hj.sievert@gmail.com>
2020-03-10 21:56:59 -0700

    refactor RegisterProvider

--- Screens/Account/aRegProvider.js
+++ Screens/Account/aRegProvider.js
@@ -3,17 +3,123 @@ import {
   View,
   Text,
   StyleSheet,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions,
+  Platform
 } from 'react-native';
+import PropTypes from 'prop-types';
 import { Icon } from 'react-native-elements';
-import { createStackNavigator } from '@react-navigation/stack';
 
-function aRegProvider() {
+export default class aRegProvider extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      flag: Dimensions.get('window').width * 1000 + Dimensions.get('window').height,
+      adjust: Dimensions.get('window').width > Dimensions.get('window').height && Platform.OS !== 'web',
+    }
+  }
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Register Provider</Text>
-    </View>)
+  componentDidMount() {
+    Dimensions.addEventListener('change', this._handleDimChange);
+    //console.log('aRegProvider componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('aRegCheck will unmount');
+    Dimensions.removeEventListener('change', this._handleDimChange);
+  }
+
+  _handleDimChange = ({ window }) => {
+    let flag = window.width * 1000 + window.height;
+    let adjust = window.width > window.height && Platform.OS !== 'web';
+    // console.log('pDrugSearch _handleDimChange event, new flag  = ', flag);
+    this.setState({
+      flag: flag,
+      adjust: adjust
+    })
+  }
+
+  render() {
+    const { adjust } = this.state;
+    const { navigation } = this.props;
+    return (
+      <View style={{ height: Dimensions.get('window').height - 75 - (adjust ? 0 : 35) }} >
+        <View style={{ marginTop: 10, borderColor: '#bbb', borderWidth: 1, backgroundColor: 'linen', paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }}>
+          <Text style={{ paddingBottom: 3 }}>{'Your account has been created, an email will be sent to you to complete validation. Following email verification, you have to activate the EZPartD app to get access to all features.'}</Text>
+          <Text style={{ paddingBottom: 3 }}>{'You can provide a mobile phone number to receive your validation code or you can choose to use the email address you provided.'}</Text>
+        </View>
+
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          paddingTop: 3,
+          backgroundColor: 'rgb(183, 211, 255)',
+          borderBottomWidth: 1,
+          borderBottomColor: 'black',
+          borderTopWidth: 1,
+          borderTopColor: 'black'
+        }}
+        >
+          <TouchableHighlight
+            onPress={() => navigation.navigate('aRegFinish', {
+              provider: 'Email'
+            })}
+          >
+            <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
+              <Icon
+                name={'email-outline'}
+                type={'material-community'}
+                color={'black'}
+                size={25}
+                containerStyle={{
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                }}
+              />
+              <Text
+                style={[styles.topTabText, { color: 'black' }]}
+              >
+                {'USE EMAIL'}
+              </Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => navigation.navigate('aRegPhone')}
+          >
+            <View style={{ flexDirection: 'column', justifyContent: 'space-between', paddingBottom: 5 }}>
+              <Icon
+                name={'phone-classic'}
+                type={'material-community'}
+                color={'black'}
+                size={25}
+                containerStyle={{
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                }}
+              />
+              <Text
+                style={styles.topTabText}
+              >
+                {'USE PHONE'}
+              </Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+
+      </View>)
+  }
 }
 
-export default aRegProvider;
+aRegProvider.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+
+const styles = StyleSheet.create({
+  topTabText: {
+    fontSize: 8,
+    //fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
+    paddingTop: 2,
+  },
+});
