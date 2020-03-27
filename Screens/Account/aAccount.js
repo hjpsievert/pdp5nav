cff7f5cb1bd971cd0e502cffa538b1a1c22b48d3
HJ Sievert <hj.sievert@gmail.com>
2020-03-27 14:06:16 -0700

    more cleanup

--- Screens/Account/aAccount.js
+++ Screens/Account/aAccount.js
@@ -34,7 +34,7 @@ export class aAccount extends React.Component {
 
   _buildList = () => {
     const { userProfile, updateFlowState } = this.props;
-    const { userMode, userStateId } = userProfile;
+    const { userMode } = userProfile;
     let listOptions = [];
     let i = 0;
 
@@ -48,11 +48,6 @@ export class aAccount extends React.Component {
 
     listOptions.push({ key: i++, title: 'Select State', subtitle: 'Specify your state of residence', icon: 'select1', type: 'antdesign', active: doState, target: 'aRegState', paramName: 'doClean', param: true });
     listOptions.push({ key: i++, title: 'Register', subtitle: 'Register for a new account', icon: 'user-plus', type: 'font-awesome', active: userMode === usrMode.anon || userMode === usrMode.created, target: userMode === usrMode.created ? 'aRegFinish' : 'aRegCreate' });
-    // listOptions.push({ key: i++, title: 'Register Create', subtitle: 'For test only', icon: 'user-plus', type: 'font-awesome', active: true, target: 'aRegCreate' });
-    // listOptions.push({ key: i++, title: 'Register Provider', subtitle: 'For test only', icon: 'user-plus', type: 'font-awesome', active: true, target: 'aRegProvider' });
-    // listOptions.push({ key: i++, title: 'Register Phone', subtitle: 'For test only', icon: 'user-plus', type: 'font-awesome', active: true, target: 'aRegPhone', paramName: 'provider', param: 'Phone' });
-    // listOptions.push({ key: i++, title: 'Register Finish Phone', subtitle: 'For test only', icon: 'user-plus', type: 'font-awesome', active: true, target: 'aRegFinish', paramName: 'provider', param: 'Phone' });
-    // listOptions.push({ key: i++, title: 'Register Finish Email', subtitle: 'For test only', icon: 'user-plus', type: 'font-awesome', active: true, target: 'aRegFinish', paramName: 'provider', param: 'Email' });
     listOptions.push({ key: i++, title: 'Activate', subtitle: 'Unlock the EZPartD Application', icon: 'key', type: 'font-awesome', active: userMode===usrMode.activating, target: 'aActivate' });
     listOptions.push({ key: i++, title: 'Login', subtitle: 'Login to your account', icon: 'login', type: 'material-community', active: true, target: 'aLogin' });
     listOptions.push({ key: i++, title: 'Change Password', subtitle: 'Change your password', icon: 'key-change', type: 'material-community', active: appVerified, target: 'aChangePw' });
